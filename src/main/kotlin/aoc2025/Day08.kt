bb185f9b75f0da22d5f1dfd9a03ab0dfbfe4871b
Dec <gh@declanm.xsmail.com>
2025-12-16 20:20:02 +0000

    Day 8 part 2 and refactor.

--- src/main/kotlin/aoc2025/Day08.kt
+++ src/main/kotlin/aoc2025/Day08.kt
@@ -12,15 +12,16 @@ private fun readInput(path: String) {
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
 }
 
+var circuits = mutableSetOf<MutableSet<String>>()
+var closestPairs = mutableListOf<Pair<String, Double>>()
+var count = 0
+
 private fun part1(loops: Int): Long {
 
     // Add all as single "circuits".
-    var circuits = mutableSetOf<MutableSet<String>>()
     lineList.forEach { l -> circuits.add(mutableSetOf(l)) }
-    
-    // Build the map of pairs with distance.
-    var closestPairs = mutableListOf<Pair<String, Double>>()
-    
+
+    // Build the list of pairs with distance.
     for (i in 0..<lineList.size) {
         for (j in i + 1..<lineList.size) {
             val distance = compareDistances(lineList[i], lineList[j])
@@ -28,26 +29,16 @@ private fun part1(loops: Int): Long {
         }
     }
     println("Distances calculated for ${closestPairs.size} pairs.")
-    
+
     // Sort on distance.
-    closestPairs = closestPairs.sortedBy { (_,v) -> v }.toMutableList()
+    closestPairs = closestPairs.sortedBy { (_, v) -> v }.toMutableList()
     println("Distances sorted for ${closestPairs.size} pairs.")
 
-    var count = 0
     while (count < loops) {
-        val (pointA,pointB) = closestPairs[count].first.split('|')
-        
-        // Fold circuits.
-        val dups = circuits.filter { it.contains(pointA) || it.contains(pointB) }.sortedBy { it.size }.reversed()
-        for (d in 1..<dups.size) {
-            dups[0].addAll(dups[d])
-            dups[d].clear()
-        }
-        // Discard the empty sets.
-        circuits = circuits.filter { it.size >0 }.toMutableSet()
+        foldCircuits(count)
         count++
     }
-    
+
     // Sort circuits by size and multiply top 3 for answer.
     var total = 1L
     val t = circuits.sortedBy { l -> l.size }.reversed()
@@ -56,6 +47,34 @@ private fun part1(loops: Int): Long {
     }
 
     return total
+}
+
+private fun foldCircuits(count: Int) {
+    val (pointA, pointB) = closestPairs[count].first.split('|')
+
+    // Fold circuits down to largest.
+    val dups = circuits.filter { it.contains(pointA) || it.contains(pointB) }.sortedBy { it.size }.reversed()
+    for (d in 1..<dups.size) {
+        dups[0].addAll(dups[d])
+        dups[d].clear()
+    }
+    // Discard the empty sets.
+    circuits = circuits.filter { it.isNotEmpty() }.toMutableSet()
+}
+
+private fun part2(): Long {
+    while (circuits.size > 1) {
+        foldCircuits(count)
+        count++
+    }
+    count--
+    println("Single circuit at $count ${closestPairs[count]}")
+
+    // Get the x positions, multiply and return as a Long.
+    val (a, b) = closestPairs[count].first.split('|')
+    val xA = a.split(',').first().toLong()
+    val xB = b.split(',').first().toLong()
+    return xA * xB
 }
 
 private fun compareDistances(a: String, b: String): Double {
@@ -71,7 +90,7 @@ private fun compareDistances(a: String, b: String): Double {
 
 fun main(args: Array<String>) {
     readInput(args[0])
-//    println(part1(10)) // 10 for example.
+//    println(part1(10)) // 10 for example, 1000 for puzzle.
     println(part1(1000)) // 75680
-//    println(part2()) //
+    println(part2()) // 8995844880
 }
